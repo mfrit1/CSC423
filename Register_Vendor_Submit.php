e29d666241e06805c2e161defd6010c57b4ab9be
Writ3r <lucas.wing@aol.com>
2018-10-28 17:19:06 -0400

    Add files via upload

--- Register_Vendor_Submit.php
+++ Register_Vendor_Submit.php
@@ -1,18 +1,26 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+require 'DBInfo.php';
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
+$vendorId = test_input($_POST["InputVendorID"]);
+$vendorCode = test_input($_POST["InputVendorCode"]);
+$vendorName = test_input($_POST["InputVendorName"]);
+$address = test_input($_POST["InputVendorAddress"]);
+$city = test_input($_POST["InputVendorCity"]);
+$state = test_input($_POST["stateSelect"]);
+$zip = test_input($_POST["InputVendorZip"]);
+$phone = test_input($_POST["InputVendorContactNumber"]);
+$contactPersonName = test_input($_POST["InputVendorContactName"]);
+$password = test_input($_POST["InputVendorPassword"]);
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+$sql = "INSERT INTO vendor (vendorId, vendorCode, vendorName, address, city, state, zip, phone, contactPersonName, password) VALUES ('{$vendorId}','{$vendorCode}','{$vendorName}','{$address}','{$city}','{$state}','{$zip}','{$phone}','{$contactPersonName}','{$password}')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
@@ -21,4 +29,5 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
+
 ?>
