c3c0e44d46dd044f6a5766db4e7b5c1663c1507a
MichaelKarcz <MichaelAKarcz@gmail.com>
2018-10-29 22:04:26 -0400

    Update Vendor/Update_Vendor_Submit.php

---
+++ Vendor/Update_Vendor_Submit.php
@@ -0,0 +1,34 @@
+<?php
+
+require '../DBInfo.php';
+
+$vendorCode = $_POST["InputVendorCode"];
+$vendorName = $_POST["InputVendorName"];
+$address = $_POST["InputVendorAddress"];
+$city = $_POST["InputVendorCity"];
+$state = $_POST["stateSelect"];
+$zip = $_POST["InputVendorZip"];
+$phone = $_POST["InputVendorContactNumber"];
+$contactPersonName = $_POST["InputVendorContactName"];
+$password = $_POST["InputVendorPassword"];
+
+//function test_input($data) {
+ // $data = trim($data);
+ // $data = stripslashes($data);
+ // $data = htmlspecialchars($data);
+//  return $data;
+//}
+
+$sql = "UPDATE vendor SET vendorName=$vendorName, address=$address, city=$city, state=$state, zip=$zip, phone=$phone, contactPersonName=$contactPersonName, password=$password) WHERE vendorCode=$vendorCode;";
+
+if ($conn->query($sql) === TRUE) {
+    $res="Data Inserted Successfully:";
+	echo json_encode($res);
+} else {
+   $error="Not Inserted,Some Problem occur.";
+	echo json_encode($error);
+}
+
+$conn->close();
+
+?>
