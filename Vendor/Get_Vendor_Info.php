7566cd65f3d5752bd287563b7fe99adb86a035b6
Matthew Fritschi <mfrit1@u.brockport.edu>
2018-10-30 13:06:42 -0400

    Create Get_Vendor_Info.php

---
+++ Vendor/Get_Vendor_Info.php
@@ -0,0 +1,28 @@
+<?php
+require '../DBInfo.php';
+//function test_input($data) {
+ // $data = trim($data);
+ // $data = stripslashes($data);
+ // $data = htmlspecialchars($data);
+//  return $data;
+$vendorCode = $_POST["InputVendorCode"];
+
+//}
+$sql = "SELECT * FROM vendor WHERE vendorCode='{$vendorCode}'";
+echo $sql;
+//echo $sql;
+
+$result= $conn->query($sql);
+if($result->num_rows > 0)
+{
+	$row = mysqli_fetch_array($result);
+	echo implode($row);
+}
+else
+{
+	header("HTTP/1.0 404 Not Found");
+  	exit();
+}
+$conn->close();
+
+?>
