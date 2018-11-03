d4283c71af3132d9e4b8a8a3520633e0c3dfc7c7
Writ3r <lucas.wing@aol.com>
2018-11-03 16:46:55 -0400

    Add files via upload

---
+++ Vendor/Register_Vendor_Submit.php
@@ -0,0 +1,60 @@
+<?php
+
+require '../DBInfo.php';
+
+$vendorCode = $_POST["InputVendorCode"];
+$vendorName = $_POST["InputVendorName"];
+$address = $_POST["InputVendorAddress"];
+$city = $_POST["InputVendorCity"];
+$state = $_POST["stateSelect"];
+$zip = $_POST["InputVendorZip"];
+$phone = $_POST["InputVendorContactNumber"];
+$contactPersonName = $_POST["InputVendorContactName"];
+$password = randomPassword();
+
+//function test_input($data) {
+ // $data = trim($data);
+ // $data = stripslashes($data);
+ // $data = htmlspecialchars($data);
+//  return $data;
+//}
+	$sql = "SELECT * FROM vendor WHERE (vendorCode='{$vendorCode}')";
+//echo $sql;
+	$result= $conn->query($sql);
+
+if($result->num_rows == 0)
+{
+	$sql = "INSERT INTO vendor (vendorCode, vendorName, address, city, state, zip, phone, contactPersonName, password) VALUES ('{$vendorCode}','{$vendorName}','{$address}','{$city}','{$state}','{$zip}','{$phone}','{$contactPersonName}','{$password}')";
+
+	if ($conn->query($sql) === TRUE) {
+    	$res="Data Inserted Successfully:";
+		echo json_encode($res);
+	} else {
+   		$error="Not Inserted, Some Problems Occurred.";
+		echo json_encode($error);
+	}
+
+	$conn->close();
+}
+
+else
+{
+	header("HTTP/1.0 404 Not Found");
+  	exit();
+}
+
+
+
+//Function for making a password for the Vendor.
+	function randomPassword() {
+		$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    	$pass = array(); //remember to declare $pass as an array
+    	$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    	for ($i = 0; $i < 15; $i++) {
+        	$n = rand(0, $alphaLength);
+        	$pass[] = $alphabet[$n];
+    	}
+    	return implode($pass); //turn the array into a string
+	}
+
+?>
---
+++ Vendor/getVendor.php
@@ -0,0 +1,32 @@
+<?php
+
+//Sets the database connection. This file must have your own DB information inside of it.
+require '../DBInfo.php';
+
+//SQL statement to get the record.
+$sql = "SELECT * FROM vendor ORDER BY CHAR_LENGTH(vendorCode), vendorCode";
+
+//Run the SQL statement and store the returned values in result.
+$result= $conn->query($sql);
+
+
+
+
+//As long as there is another row to be processed, do the following loop. This adds all returned DB records to the table.
+while($data = mysqli_fetch_row($result))
+{   
+    echo "<tr>";
+    echo "<td scope='row'><b>$data[1]</b></td>";
+    echo "<td align=center>$data[2]</td>";
+    echo "<td align=center>$data[3]</td>";
+    echo "<td align=center>$data[4]</td>";
+    echo "<td align=center>$data[5]</td>";
+    echo "<td align=center>$data[6]</td>";
+    echo "<td align=center>$data[7]</td>";
+    echo "<td align=center>$data[8]</td>";
+    echo "<td align=center>$data[10]</td>";
+    echo "<td align=center><button type='button' class='btn btn-warning' style='padding-right: 10px'><i class='fa fa-pencil'></i></button><div class='divider'></div><button type='button' class='btn btn-danger'><i class='fa fa-trash'></i></button></td>";
+    echo "</tr>";
+}
+
+?>
