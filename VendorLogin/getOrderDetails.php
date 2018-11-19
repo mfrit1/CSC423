22a9dd60426bd3009fdc2abc512213652ae7582c
Matthew Fritschi <mfrit1@u.brockport.edu>
2018-11-19 16:26:39 -0500

    Create getOrderDetails.php

---
+++ VendorLogin/getOrderDetails.php
@@ -0,0 +1,60 @@
+<?php
+
+//Sets the database connection. This file must have your own DB information inside of it.
+require '../DBInfo.php';
+
+$trid = $_GET['orderId'];
+
+//SQL statement to get the record.
+$sql = "SELECT orders.orderId, inventoryitem.itemId, inventoryitem.description, inventoryitem.imageFileName, orderdetail.quantityOrdered
+		FROM orders
+        INNER JOIN orderdetail ON orders.orderId = orderdetail.orderId
+        INNER JOIN inventoryitem ON orderdetail.itemId = inventoryitem.itemId
+		WHERE orders.orderId = $trid
+		ORDER BY orders.orderId";
+
+//Run the SQL statement and store the returned values in result.
+$result= $conn->query($sql);
+
+echo '
+<div class="col-lg-1">
+      </div>
+    <div class="col-lg-10" style="text-align: center">
+          <div style="text-align: center; overflow-y:auto; max-height: 750px;" >
+            <table class="table table-bordered" style="max-width: 100% " align="center" >
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">Order ID</th>
+                  <th scope="col">Item ID</th>
+                  <th scope="col">Item Description</th>
+                  <th scope="col">Item Image</th>
+                  <th scope="col">Quantity</th>
+                </tr>
+              </thead>
+              <tbody id = "itemTable" style="background-color: white;">';
+
+//As long as there is another row to be processed, do the following loop. This adds all returned DB records to the table.
+while($data = mysqli_fetch_row($result))
+{   
+    echo "<tr id='$data[0]' height='130px'>";
+    echo "<td scope='row' style='padding-top: 50px'><b>$data[0]</b></td>";
+    echo "<td align=center  style='padding-top: 50px'>$data[1]</td>";
+    echo "<td align=center><img src='../FD Images/$data[2]' height='125px' width='125px' class='zoom'></td>";
+    echo "<td align=center  style='padding-top: 50px'>$data[3]</td>";
+    echo "<td align=center style='padding-top: 45px'><button type='button' class='btn btn-warning' style='padding-right: 10px' onclick='modifyItem($(this))'><i class='fa fa-pencil'></i></button><div class='divider'></div><button type='button' class='btn btn-danger' onclick='deleteItem($(this))'><i class='fa fa-trash'></i></button></td>";
+    echo "</tr>";
+}
+
+echo '
+</tbody>
+            </table>
+
+          </div>
+      <!-- <div class="col-lg-1"></div> -->
+      </div>
+      <div class="col-lg-1">
+      </div>
+      </div>
+      ';
+
+?>
