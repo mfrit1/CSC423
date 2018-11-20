e8022a0e24a4b6d9c971ccb0f5cc6430385b94ef
Matthew Fritschi <mfrit1@u.brockport.edu>
2018-11-20 14:31:59 -0500

    Update modifyItem.php

--- Items/modifyItem.php
+++ Items/modifyItem.php
@@ -23,45 +23,45 @@ echo '<div class = "container">
         
       </div>
     </div>
-    <form class="form-horizontal" method="post" id = "myForm" action = "Modify_Item_Submit.php?id=' .  $data[0]  .'" >
+    <form class="form-horizontal" method="post" id = "myForm" action = "Modify_Item_Submit.php?id=' .  $data[0]  .'">
       <div style="height: 25px"></div>
       <div class="form-group row">
         <div class ="col-lg-3">
           <label for="itemId">Item Number</label>
-          <input type="text" class="form-control" name="itemId" id="number" value="' . $data[0] . '">
+          <input type="text" class="form-control" name="itemId" id="number" value="' . $data[0] . '" required>
         </div>
         <div class ="col-lg-9">
           <label for="itemDepartment">Department</label>
-          <input type="text" class="form-control" name="itemDepartment" id="itemDepartment" value="' . $data[4] . '">
+          <input type="text" class="form-control" name="itemDepartment" id="itemDepartment" value="' . $data[4] . '" required>
         </div>
       </div>
       <div style="height: 25px"></div>
       <div class="form-group row">
         <div class ="col-lg-4">
           <label for="itemSize">Size</label>
-          <input type="text" class="form-control" name="itemSize" id="Size" value="' . $data[2] . '">
+          <input type="text" class="form-control" name="itemSize" id="Size" value="' . $data[2] . '" required>
         </div>
         <div class ="col-lg-4">
           <label for="itemCost">Item Cost</label>
-          <input type="text" class="form-control" name="itemCost" id="itemCost" value="'  . $data[6] .  '">
+          <input type="text" class="form-control" name="itemCost" id="itemCost" value="'  . $data[6] .  '" required>
         </div>
         <div class ="col-lg-4">
           <label for="retailPrice">Retail Price</label>
-          <input type="text" class="form-control" name="retailPrice" id="retailPrice" value="' . $data[7] . '">
+          <input type="text" class="form-control" name="retailPrice" id="retailPrice" value="' . $data[7] . '" required>
         </div>
       </div>
       <div style="height: 25px"></div>
       <div class="form-group row">
         <div class ="col-lg-12">
           <label for="itemDescription">Description</label>
-          <input type="text" class="form-control" name="itemDescription" id="Description" value="' . $data[1] . '">
+          <input type="text" class="form-control" name="itemDescription" id="Description" value="' . $data[1] . '" required>
         </div>
       </div>
       <div style="height: 25px"></div>
       <div class="form-group row">
         <div class ="col-lg-4">
           <label for="divisionDropdown">Division</label>
-          <select class="form-control" name="divisionDropdown" id="' . $data[3] . '">
+          <select class="form-control" name="divisionDropdown" id="' . $data[3] . '" required>
           <option value="" selected>Select a Division</option>
 
 
@@ -72,7 +72,7 @@ echo '<div class = "container">
         </div>
         <div class ="col-lg-4">
           <label for="categoryDropdown">Category</label>
-          <select class="form-control" name="categoryDropdown" id="' . $data[5] . '">
+          <select class="form-control" name="categoryDropdown" id="' . $data[5] . '" required>
           <option value="" selected>Select a Category</option>
 
 
@@ -82,7 +82,7 @@ echo '<div class = "container">
         </div>
         <div class ="col-lg-4">
           <label for="vendorDropdown">Vendor</label>
-          <select class="form-control" name="vendorDropdown" id="'  . $data[9] . '">
+          <select class="form-control" name="vendorDropdown" id="'  . $data[9] . '" required>
           <option value="" selected>Select a Vendor</option>
 
 
@@ -95,7 +95,7 @@ echo '<div class = "container">
       <div class="form-group row">
         <div class ="col-lg-8">
           <label for="itemImagePath" style="padding-top: 10%">Image File</label>
-          <input type="text" class="form-control" name="itemImagePath" id="itemImagePath" value="' . $data[8] . '" onkeyup="getImage()">
+          <input type="text" class="form-control" name="itemImagePath" id="itemImagePath" value="' . $data[8] . '" onkeyup="getImage()" required>
         </div>
         <div class ="col-lg-4">
           <img src="../FD Images/' . $data[8] . '" id="itemImage">
