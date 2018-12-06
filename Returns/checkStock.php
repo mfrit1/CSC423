fdfcd9fcda30075c00001ab13b37b1c1b36c9884
nbarn3 <35875427+nbarn3@users.noreply.github.com>
2018-12-06 14:57:04 -0500

    Update checkStock.php

--- Returns/checkStock.php
+++ Returns/checkStock.php
@@ -3,12 +3,17 @@
 //Sets the database connection. This file must have your own DB information inside of it.
 require '../DBInfo.php';
 
-$storeCode = $_POST["storeCode"];
 $itemId = $_POST["itemId"];
+$storeCode = $_POST["storeCode"];
+
+$sql = "SELECT storeId FROM retailstore where storeCode = '{$storeCode}'";
+$result= $conn->query($sql);
+$data = mysqli_fetch_row($result);
+$storeId = $data[0];
+
 
 //SQL statement to get the record.
-$sql = "SELECT quantityInStock FROM inventory WHERE storeId = '{$storeCode}' AND itemId = '{$itemId}' ";
-
+$sql = "SELECT quantityInStock FROM inventory WHERE storeId = '{$storeId}' AND itemId = '{$itemId}' ";
 
 //Run the SQL statement and store the returned values in result.
 $result= $conn->query($sql);
