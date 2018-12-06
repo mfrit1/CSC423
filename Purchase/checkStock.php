c76b0f96de8cbf76fe5b2bb6d098bb9956c52039
Neesons <34045289+Neesons@users.noreply.github.com>
2018-12-05 22:22:47 -0500

    Add files via upload

--- Purchase/checkStock.php
+++ Purchase/checkStock.php
@@ -3,18 +3,20 @@
 //Sets the database connection. This file must have your own DB information inside of it.
 require '../DBInfo.php';
 
+$storeCode = $_POST["storeCode"];
+$itemId = $_POST["itemCode"];
+
 //SQL statement to get the record.
-$sql = "SELECT quantityInStock FROM inventory WHERE storeId  ";
+$sql = "SELECT quantityInStock FROM inventory WHERE storeId = '{$storeCode}' AND itemId = '{$itemId}' ";
+
 
 //Run the SQL statement and store the returned values in result.
 $result= $conn->query($sql);
 
-echo '<option value="" disabled selected hidden>Stores...</option>"';
-
 //As long as there is another row to be processed, do the following loop. This adds all returned DB records to the table.
 while($data = mysqli_fetch_row($result))
 {   
-    echo "<option value='$data[0]'>$data[1]</option>";
+    echo $data[0];
 }
 
 ?>
