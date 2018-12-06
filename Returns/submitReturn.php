733ba474e3dc35f1759c567fd055aea37cfa5428
nbarn3 <35875427+nbarn3@users.noreply.github.com>
2018-12-06 14:56:43 -0500

    Update submitReturn.php

--- Returns/submitReturn.php
+++ Returns/submitReturn.php
@@ -2,34 +2,36 @@
 
 require '../DBInfo.php';
 
-$returnToVendorId = $_POST["returnToVendorId"];
+//$returnToVendorId = $_POST["returnToVendorId"];
 $vendorCode = $_POST["vendorCode"];
 $storeCode = $_POST["storeCode"];
 $totalIds = $_POST["idCount"];
 $todayDate = $_POST["date"];
+$itemId = $_POST["itemId"];
+$qReturned = $_POST["qReturned"];
+$updatedStock = $_POST["newStock"];
 
-
-if(isset($_POST["returnToVendorId"]))
-{
-	$sql = "DELETE FROM returntovendordetail WHERE returnToVendorId = '{$returnToVendorId}'";
-	$result= $conn->query($sql);
+// if(isset($_POST["returnToVendorId"]))
+// {
+// 	$sql = "DELETE FROM returntovendordetail WHERE returnToVendorId = '{$returnToVendorId}'";
+// 	$result= $conn->query($sql);
 	
-	for($i = 1; $i <= $totalIds; $i++){
-		$item = $_POST["ID" . $i];
-		$quantity = $_POST["Quant" . $i];
-		$sql = "INSERT INTO returntovendordetail (returnToVendorId, itemId, quantityReturned) VALUES ('{$returnToVendorId}','{$item}','{$quantity}')";
+// 	for($i = 1; $i <= $totalIds; $i++){
+// 		$item = $_POST["ID" . $i];
+// 		$quantity = $_POST["Quant" . $i];
+// 		$sql = "INSERT INTO returntovendordetail (returnToVendorId, itemId, quantityReturned) VALUES ('{$returnToVendorId}','{$item}','{$quantity}')";
 		
-		if ($conn->query($sql) === TRUE) {
-			$res="Data Inserted Successfully:";
-			echo json_encode($res);
-			} else {
-			$error="Not Inserted, Some Problems Occurred.";
-			echo json_encode($error);
-		}
+// 		if ($conn->query($sql) === TRUE) {
+// 			$res="Data Inserted Successfully:";
+// 			echo json_encode($res);
+// 			} else {
+// 			$error="Not Inserted, Some Problems Occurred.";
+// 			echo json_encode($error);
+// 		}
 		
-	}
-}
-else{
+// 	}
+//}
+//else{
 
 	$sql = "SELECT vendorId FROM vendor where vendorCode = '{$vendorCode}'";
 	$result= $conn->query($sql);
@@ -40,7 +42,6 @@ else{
 	$result= $conn->query($sql);
 	$data = mysqli_fetch_row($result);
 	$storeId = $data[0];
-
 
 
 	$sql = "INSERT INTO returntovendor (vendorId, storeId, dateTimeOfReturn) VALUES ('{$vendorId}','{$storeId}','{$todayDate}')";
@@ -56,13 +57,14 @@ else{
 	$sql = "SELECT MAX(returnToVendorId) FROM returntovendor";
 	$result= $conn->query($sql);
 	$data = mysqli_fetch_row($result);
-	$orderId = $data[0];
+	$returnToVendorId = $data[0];
 
-	for($i = 1; $i <= $totalIds; $i++){
-		$item = $_POST["ID" . $i];
-		$quantity = $_POST["Quant" . $i];
-		$sql = "INSERT INTO returntovendordetail (returnToVendorId, itemId, quantityReturned) VALUES ('{$returnToVendorId}','{$item}','{$quantity}')";
+	//for($i = 1; $i <= $totalIds; $i++){
+	
+		$sql = "INSERT INTO returntovendordetail (returnToVendorId, itemId, quantityReturned) VALUES ('{$returnToVendorId}','{$itemId}','{$qReturned}')";
 		
+		echo($sql);
+
 		if ($conn->query($sql) === TRUE) {
 			$res="Data Inserted Successfully:";
 			echo json_encode($res);
@@ -72,11 +74,10 @@ else{
 		}
 		
 		
-	}
+	//
 
-	$updatedStock = $_POST["newStock"];
 
-$sql = "UPDATE inventory SET quantityInStock ='{$updatedStock}' WHERE storeId = '{$storeCode}' AND itemId = '{$itemCode}'";
+$sql = "UPDATE inventory SET quantityInStock ='{$updatedStock}' WHERE storeId = '{$storeId}' AND itemId = '{$itemCode}'";
 
 if ($conn->query($sql) === TRUE) {
     	$res="Data Inserted Successfully:";
@@ -85,7 +86,7 @@ if ($conn->query($sql) === TRUE) {
    		$error="Not Inserted, Some Problems Occurred.";
 		echo json_encode($error);
 	}
-}
+//}
 $conn->close();
 
 ?>
