ec25d2358277576bae689cd6b468b7e9b5042793
nbarn3 <35875427+nbarn3@users.noreply.github.com>
2018-12-06 13:30:31 -0500

    Update submitReturn.php

--- Returns/submitReturn.php
+++ Returns/submitReturn.php
@@ -73,6 +73,18 @@ else{
 		
 		
 	}
+
+	$updatedStock = $_POST["newStock"];
+
+$sql = "UPDATE inventory SET quantityInStock ='{$updatedStock}' WHERE storeId = '{$storeCode}' AND itemId = '{$itemCode}'";
+
+if ($conn->query($sql) === TRUE) {
+    	$res="Data Inserted Successfully:";
+		echo json_encode($res);
+	} else {
+   		$error="Not Inserted, Some Problems Occurred.";
+		echo json_encode($error);
+	}
 }
 $conn->close();
 
